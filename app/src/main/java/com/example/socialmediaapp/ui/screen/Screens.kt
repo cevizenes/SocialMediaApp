7ac3e83ee3b46cc944c57cfec7456a4da5903637
ceviz <402498@cs.ktu.edu.tr>
2023-05-25 12:52:22 +0300

    Added NavGraph

--- app/src/main/java/com/example/socialmediaapp/ui/screen/Screens.kt
+++ app/src/main/java/com/example/socialmediaapp/ui/screen/Screens.kt
@@ -5,21 +5,32 @@ import androidx.compose.material.icons.filled.ViewComfyAlt
 import androidx.compose.material.icons.filled.ViewTimeline
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class BottomBarScreen(
+sealed class Screens(
     val route : String,
     val title : String,
-    val icon : ImageVector
+    val icon : ImageVector?
 ) {
-    object Post : BottomBarScreen(
+    object Post : Screens(
         route = "posts",
         title = "Posts",
         icon = Icons.Filled.ViewTimeline
     )
 
-    object Album : BottomBarScreen(
+    object Album : Screens(
         route = "albums",
         title = "Albums",
         icon = Icons.Filled.ViewComfyAlt
     )
 
+    object Comments : Screens(
+        route = "comments",
+        title = "comments",
+        icon =   null
+    )
+
+    object Photos : Screens(
+        route = "photos",
+        title = "photos",
+        icon = null
+    )
 }
